c060b5ccd3ab822dea33f642defd31af330e7035
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2020-01-13 19:32:09 +0530

    io insert a value at a particular index and remaining array is shifted forward

--- src/arrayTask/Task2.java
+++ src/arrayTask/Task2.java
@@ -1,63 +1,79 @@
 package arrayTask;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Task2 {
-    static Scanner sc = new Scanner(System.in);
+    //static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int[] array = new int[10];
-        insertValuesIntoArray(array);
-        insertValueAtGivenIndex(array);
-        boolean response = searchTheElement(array);
-        if (response) {
-            System.out.println("found");
-        } else {
-            System.out.println("not found");
-        }
+        int[] array = new int[5];
+        Scanner sc = new Scanner(System.in);
+        printValueOfArray(array);
+        insertValuesIntoArray(array, sc);
+        printValueOfArray(array);
+        System.out.println("enter the index");
+        int index = sc.nextInt();
+        System.out.println("enter the value");
+        int value1 = sc.nextInt();
+        insertValueAtGivenIndex(array, value1, index);
+        //insertValueAtGivenIndex(array,sc.nextInt(),sc.nextInt());
+        printValueOfArray(array);
+        searchTheElement(array, sc);
         sortedArray(array);
+        printValueOfArray(array);
+    }
+
+
+    public static void printValueOfArray(int[] array) {
+        System.out.println(Arrays.toString(array));
     }
 
     public static void sortedArray(int[] array) {
-        int swp;
-        for (int i = 0; i < 10; i++) {
-            for (int j = i; j < 10; j++) {
-                if (array[i] > array[j]) {
-                    swp = array[i];
-                    array[i] = array[j];
-                    array[j] = swp;
-                }
-            }
-        }
+        Arrays.sort(array);
     }
 
-    public static boolean searchTheElement(int[] array) {
+    public static void searchTheElement(int[] array, Scanner sc) {
         System.out.println("enter the to find in the array");
         int value = sc.nextInt();
         boolean response = false;
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
             if (array[i] == value) {
                 response = true;
                 break;
             }
         }
-        return response;
+        response(response);
+    }
+
+    public static void response(boolean response) {
+        if (response) {
+            System.out.println("found");
+        } else {
+            System.out.println("not found");
+        }
     }
 
     /**
      * insertValueAtGivenIndex function is used to insert value at a particular index.
      */
 
-    public static void insertValueAtGivenIndex(int[] array) {
-        System.out.println("enter the value");
-        int value = sc.nextInt();
-        System.out.println("enter the index");
-        int index = sc.nextInt();
+    public static void insertValueAtGivenIndex(int[] array, int value1, int index) {
+
+
+        shiftRight(array, value1, index);
+        printValueOfArray(array);
+    }
+
+    public static void shiftRight(int[] array, int value, int index) {
+        for (int i = array.length - 1; i > index; i--) {
+            array[i] = array[i - 1];
+        }
         array[index] = value;
     }
 
-    public static void insertValuesIntoArray(int[] array) {
-        for (int i = 0; i < 9; i++) {
+    public static void insertValuesIntoArray(int[] array, Scanner sc) {
+        for (int i = 0; i < 5; i++) {
             array[i] = sc.nextInt();
         }
     }
